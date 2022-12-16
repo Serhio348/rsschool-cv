d677a6d9144a839c0b77114a8d11a00aac70a56a
Сидорович Сергей <D:\The Bat!>
2022-12-16 15:46:23 +0300

    feat: add animation and script file

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+let text1 = 'I am 35. I live in Brest, Belarus. I develop and manage websites for 1 years.'
+let text2 = 'Im an adventure lover taking my good old Land Cruiser to off-road trails'
+let text3 = 'Im a Marvel fan and a father of 2 awesome kids!'
+let content = document.getElementById('content');
+
+for (let i in [...text1]) {
+    let letter = document.createElement('span')
+    letter.textContent = [...text1][i]
+    if (letter.textContent.match(/\s/)) {
+        letter.style.margin = 'auto 3px'
+    }
+    letter.style.animationDelay = i / 100 + 's'
+    content.appendChild(letter)
+    
+}
+
+setTimeout(() => {
+    for (let i in [...text2]) {
+        let letter = document.createElement('span')
+        letter.textContent = [...text2][i]
+        if (letter.textContent.match(/\s/)) {
+            letter.style.margin = 'auto 3px'
+        }
+        letter.style.animationDelay = i / 100 + 's'
+        content.appendChild(letter)
+      
+    }
+}, 2000)
+
+setTimeout(() => {
+    for (let i in [...text3]) {
+        let letter = document.createElement('span')
+        letter.textContent = [...text3][i]
+        if (letter.textContent.match(/\s/)) {
+            letter.style.margin = 'auto 3px'
+        }
+        letter.style.animationDelay = i / 100 + 's'
+        content.appendChild(letter)
+        
+    }
+}, 4000)
+
